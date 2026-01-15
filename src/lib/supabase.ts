e88f8498989ddcefd45430bdbbb1914aafccc580
Abiodun “Abbey” Aina <olamilekansunday445@gmail.com>
2026-01-15 22:49:05 +0000

    Use environment variables for your Supabase keys

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Initialize Supabase client
-const supabaseUrl = 'https://xjvjwtemjigpbvikzxwi.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhqdmp3dGVtamlncGJ2aWt6eHdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1MDkyMTMsImV4cCI6MjA4NDA4NTIxM30.gXXNel0xFwgCuSiGViGiyoj65nTJNOFtpzU-hXr_cfI';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
