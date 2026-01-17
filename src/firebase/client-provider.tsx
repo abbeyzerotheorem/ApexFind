30c9572931fc62b55e45a2480b8aa58fb3f7c489
Abiodun “Abbey” Aina <olamilekansunday445@gmail.com>
2026-01-17 18:46:32 +0000

    Firebase: Error (auth/network-request-failed).

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,24 +1,21 @@
 'use client';
 
-import {useEffect, useState} from 'react';
-
-import {initializeApp, type FirebaseOptions} from 'firebase/app';
-import {connectAuthEmulator, getAuth} from 'firebase/auth';
-import {connectFirestoreEmulator, getFirestore} from 'firebase/firestore';
-
+import {useState} from 'react';
+import {initializeApp, getApps, type FirebaseOptions} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getFirestore} from 'firebase/firestore';
 import {FirebaseProvider} from './provider';
 
 /**
- * Initializes a Firebase app instance and returns the emulated
- * services.
+ * Initializes a Firebase app instance and returns the services.
+ * It ensures that Firebase is initialized only once.
  */
-function initializeEmulatedFirebase(options: FirebaseOptions) {
-  const app = initializeApp(options);
+function initializeFirebase(options: FirebaseOptions) {
+  // Check if Firebase has already been initialized
+  const existingApp = getApps().length ? getApps()[0] : null;
+  const app = existingApp || initializeApp(options);
   const firestore = getFirestore(app);
   const auth = getAuth(app);
-
-  connectFirestoreEmulator(firestore, '127.0.0.1', 8080);
-  connectAuthEmulator(auth, 'http://127.0.0.1:9099');
 
   return {
     app,
@@ -30,8 +27,6 @@ function initializeEmulatedFirebase(options: FirebaseOptions) {
 /**
  * A client-side provider that initializes Firebase and provides it to the
  * rest of the application.
- *
- * It is responsible for ensuring that Firebase is initialized only once.
  */
 export function FirebaseClientProvider({
   children,
@@ -40,9 +35,7 @@ export function FirebaseClientProvider({
   children: React.ReactNode;
   options: FirebaseOptions;
 }) {
-  const [firebase, setFirebase] = useState(() =>
-    initializeEmulatedFirebase(options)
-  );
+  const [firebase] = useState(() => initializeFirebase(options));
 
   return (
     <FirebaseProvider
