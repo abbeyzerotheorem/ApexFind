7c24f06db39646044c9f9ff85f62f2717c71f780
Abiodun “Abbey” Aina <olamilekansunday445@gmail.com>
2026-01-17 13:57:10 +0000

    Try fixing this error: `Runtime ReferenceError: createServerClient is no

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 
 import { type NextRequest, NextResponse } from 'next/server'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
@@ -16,11 +17,6 @@ export async function middleware(req: NextRequest) {
           return req.cookies.get(name)?.value
         },
         set(name: string, value: string, options: CookieOptions) {
-          req.cookies.set({
-            name,
-            value,
-            ...options,
-          })
           res.cookies.set({
             name,
             value,
@@ -28,8 +24,11 @@ export async function middleware(req: NextRequest) {
           })
         },
         remove(name: string, options: CookieOptions) {
-          req.cookies.delete(name)
-          res.cookies.delete(name)
+          res.cookies.set({
+            name,
+            value: '',
+            ...options,
+          })
         },
       },
     }
