19c4815cc6681cf2fdc62d71203576b938125060
Abiodun “Abbey” Aina <olamilekansunday445@gmail.com>
2026-01-19 14:58:38 +0000

    Uncaught Error: Invalid src prop (https://unsplash.com/photos/white-conc

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
