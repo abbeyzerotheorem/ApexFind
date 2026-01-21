208109d458bb7d722c7a24e615005a25d471a92a
Abiodun “Abbey” Aina <olamilekansunday445@gmail.com>
2026-01-21 21:37:46 +0000

    ## Error Type Runtime Error
    ## Error Message
    Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
